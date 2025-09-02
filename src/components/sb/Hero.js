53f20a4bfcb70000374073d87e33182cbfde7d13
Mark Taratynov <mark.taratynov@gmail.com>
2025-09-02 19:27:22 +0200

    refactor: improve Hero component layout and styling for better user experience style: clean up import statements and enhance readability in Page and ServerComponent

--- src/components/sb/Hero.js
+++ src/components/sb/Hero.js
@@ -4,27 +4,43 @@ export default function Hero({ blok }) {
   console.log(blok.background_image);
   console.log("HERO", blok);
 
-  let heroClasses = `h-[100vh] bg-gray-100/90 flex flex-col justify-center items-center gap-12`;
+  let heroClasses = `min-h-screen bg-gray-100/90 flex flex-col justify-center items-center gap-10 `;
 
   console.log(blok.background_image?.filename);
 
   return (
     <div {...storyblokEditable(blok)} className={heroClasses}>
-      <h1 className="text-5xl text-center font-extrabold text-black">
-        {blok.title}
-      </h1>
-      <h3 className="text-gray-500/80 text-[28px] text-center w-[70%]">
-        {blok.subtitle}
-      </h3>
-      {blok.content && (
-        <h4 className="w-[80%] text-center text-foreground">{blok.content}</h4>
-      )}
-      <a
-        href={blok.cta_link}
-        className="border border-black px-4 py-3 text-secondary inline-block w-[194px] h-[50px] text-center"
-      >
-        {blok.cta_text}
-      </a>
+      <div className="flex flex-col items-center gap-8 mt-12 w-[75%]">
+        <h1 className="text-6xl text-center font-normal text-black">
+          {blok.title}
+        </h1>
+        <h3 className="text-gray-500/80 text-[28px] text-center w-[70%]">
+          {blok.subtitle}
+        </h3>
+        {blok.content ? (
+          <h4 className="w-[80%] text-center text-foreground">
+            {blok.content}
+          </h4>
+        ) : (
+          ""
+        )}
+        {blok.cta_text && blok.cta_link && (
+          <div className="text-center mt-6">
+            <a
+              href={blok.cta_link}
+              className="border border-black px-4 py-3 text-secondary inline-block w-[194px] h-[50px] text-center hover:bg-gray-600 hover:text-white duration-100"
+            >
+              {blok.cta_text}
+            </a>
+          </div>
+        )}
+        {blok.background_image?.filename && (
+          <img
+            src={blok.background_image.filename}
+            className="w-full md:w-[90%] mb-12"
+          />
+        )}
+      </div>
     </div>
   );
 }
--- src/components/sb/Page.js
+++ src/components/sb/Page.js
@@ -1,15 +1,14 @@
-import {
-    storyblokEditable,
-    } from '@storyblok/react/rsc';
+import { storyblokEditable } from "@storyblok/react/rsc";
 
-import ServerComponent from './ServerComponent';
-    
-    export default function Page({ blok }) {
-    return (
-        <main {...storyblokEditable(blok)}>
-            {blok.body.map((nestedBlok) => (
-                <ServerComponent blok={nestedBlok} key={nestedBlok._uid} />
-            ))}
-        </main>
-    );
-    }
+import ServerComponent from "./ServerComponent";
+
+export default function Page({ blok }) {
+  return (
+    <main {...storyblokEditable(blok)}>
+      {Array.isArray(blok.body) &&
+        blok.body.map((nestedBlok) => (
+          <ServerComponent blok={nestedBlok} key={nestedBlok._uid} />
+        ))}
+    </main>
+  );
+}
