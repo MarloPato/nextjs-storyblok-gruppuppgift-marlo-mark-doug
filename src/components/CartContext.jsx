a9d711812f7f21613c79b737d298c3f0c3960595
Mark Taratynov <mark.taratynov@gmail.com>
2025-09-07 11:09:00 +0200

    refactor: improve code formatting and consistency in CartContext, CallToAction, and CartIcon components

--- src/components/CartContext.jsx
+++ src/components/CartContext.jsx
@@ -1,40 +1,41 @@
-"use client"
+"use client";
 
 import { createContext, useContext, useState } from "react";
 
 const CartContext = createContext({
-    items: []
-})
+  items: [],
+});
 
 export const useCartContext = () => useContext(CartContext);
 
-export function CartProvider({ children}) {
+export function CartProvider({ children }) {
+  const [items, setItems] = useState([]);
 
-    const [items, setItems] = useState([])
+  const addItem = (item) => {
+    setItems([...items, item]);
+  };
 
-    const addItem = (item) => {
-        setItems([...items, item])
-    }
+  const removeItem = (item) => {
+    const next = [...items];
+    const i = next.findIndex((it) => it._uid == item._uid);
+    next.splice(i, 1);
+    setItems(next);
+  };
 
-    const removeItem = (item) => {
-        const next = [...items]
-        const i = next.findIndex(it => it._uid == item._uid)
-        next.splice(i, 1)
-        setItems(next)
-    }
+  const removeAllOfItem = (item) => {
+    setItems(
+      items.filter((it) => {
+        item._uid !== it._uid;
+      })
+    );
+  };
 
-    const removeAllOfItem = (item) => {
-        setItems(items.filter(it => {
-            item._uid !== it._uid
-        }))
-    }
+  const ctx = {
+    items,
+    addItem,
+    removeItem,
+    removeAllOfItem,
+  };
 
-    const ctx = {
-        items,
-        addItem,
-        removeItem,
-        removeAllOfItem
-    }
-
-    return <CartContext.Provider value={ctx}>{children}</CartContext.Provider>
+  return <CartContext.Provider value={ctx}>{children}</CartContext.Provider>;
 }
