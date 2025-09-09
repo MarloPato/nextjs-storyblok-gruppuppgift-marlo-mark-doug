55f58276ee3ae3aeed9a99ba47e6905a299d4fe5
Mark Taratynov <mark.taratynov@gmail.com>
2025-09-09 15:48:37 +0200

    Created robots.txt, implemented some changes at sitemap.js with variable

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,50 +1,48 @@
 import { getStoryblokApi } from "../lib/storyblok.js";
-const URL = "https://nextjs-storyblok-gruppuppgift-marlo.vercel.app"
-
+import SETTINGS from "@/settings";
 
 async function fetchProducts() {
-    const storyblokApi = getStoryblokApi();
-    const story = await storyblokApi.get("cdn/stories/shoplist", {
+  const storyblokApi = getStoryblokApi();
+  const story = await storyblokApi.get("cdn/stories/shoplist", {
     version: process.env.NEXT_PUBLIC_STORYBLOK_VERSION || "published",
-    });
-    const blocks = story?.data?.story?.content?.body;
-    const productList = blocks?.find(
+  });
+  const blocks = story?.data?.story?.content?.body;
+  const productList = blocks?.find(
     (b) => b.component === "shoplistProductList"
-    );
+  );
 
-    return productList.products
+  return productList.products;
 }
 
-const products = await fetchProducts()
+const products = await fetchProducts();
 
 export default function sitemap() {
+  console.log(products.length);
 
-    console.log(products.length)
-
-    return [
-        {
-            url: `${URL}`,
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 1
-        },
-        {
-            url: `${URL}/about`,
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.8
-        },
-        {
-            url: `${URL}/shoplist`,
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.8
-        },
-        ...products.map(p => ({
-            url: `${URL}/product/${p._uid}`,
-            lastModified: new Date(),
-            changeFrequency: 'daily',
-            priority: 0.6
-        }))
-    ]
+  return [
+    {
+      url: `${SETTINGS.SITE_URL}`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${SETTINGS.SITE_URL}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${SETTINGS.SITE_URL}/shoplist`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    ...products.map((p) => ({
+      url: `${SETTINGS.SITE_URL}/product/${p._uid}`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.6,
+    })),
+  ];
 }
---
+++ src/settings.js
@@ -0,0 +1,7 @@
+const SETTINGS = {
+  SITE_URL:
+    process.env.NEXT_PUBLIC_SITE_URL ||
+    "https://nextjs-storyblok-gruppuppgift-marlo.vercel.app",
+};
+
+export default SETTINGS;
